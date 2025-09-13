c4cdc978d956ba0075a2374294ad8a6a6de4b0fe
Emmet Lappi <emlappi@edu.lapinamk.fi>
2025-09-13 15:58:43 +0300

    Kokeillaan

---
+++ d&d-nopat.py
@@ -0,0 +1,9 @@
+import random
+from operator import truediv
+
+#let´s ask from the user what and how many of the selected die they want to use
+dice = int(input("How many sides is on your die?:\n"))
+times = int(input("How many of dices do you want to use:?\n"))
+
+# there are dices with unlimited amount of sides
+sides = random.randint(1,dice)
