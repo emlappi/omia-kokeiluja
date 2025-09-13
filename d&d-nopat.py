645ba227e9a16d5ee8c18e33df4b0716b699e445
Emmet Lappi <emlappi@edu.lapinamk.fi>
2025-09-13 16:17:05 +0300

    crated a for loop, kinda

--- d&d-nopat.py
+++ d&d-nopat.py
@@ -6,7 +6,9 @@ dice = int(input("How many sides is on your die?:\n"))
 times = int(input("How many of dices do you want to use:?\n"))
 
 # there are dices with unlimited amount of sides
-sides = random.randint(1,dice)
+amount = random.randint(1,dice)
 
-#we will use for loop
-for loop
+# we will use for loop
+for in range(1,times):
+    print(dice)
+
