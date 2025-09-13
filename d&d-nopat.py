cb67be866495796bf28e32859ba2ff587bd28460
Emmet Lappi <emlappi@edu.lapinamk.fi>
2025-09-13 16:33:48 +0300

    nonniin ny pitäis olla hyvä

--- d&d-nopat.py
+++ d&d-nopat.py
@@ -3,7 +3,7 @@ from operator import truediv
 
 #let´s ask from the user what and how many of the selected die they want to use
 dice = int(input("How many sides is on your die?:\n"))
-times = int(input("How many of dices do you want to use:?\n"))
+times = int(input("How many dices do you want to use:?\n"))
 
 # we will use for loop that was suggested by my fiance
 for x in range(times):
