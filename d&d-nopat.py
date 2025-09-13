380edc88e33fe0f29b27fd4d9db08a8760fef1ad
Emmet Lappi <emlappi@edu.lapinamk.fi>
2025-09-13 16:38:24 +0300

    lisäillään kysymystä uudestaan opelaamisesta

--- d&d-nopat.py
+++ d&d-nopat.py
@@ -2,10 +2,13 @@ import random
 from operator import truediv
 
 #let´s ask from the user what and how many of the selected die they want to use
-dice = int(input("How many sides is on your die?:\n"))
-times = int(input("How many dices do you want to use:?\n"))
+dice = int(input("How many sides is on your die?\n"))
+times = int(input("How many dices do you want to use?\n"))
 
 # we will use for loop that was suggested by my fiance
 for x in range(times):
     amount = random.randint(1, dice)
     print(f"Dice {x+1} gave {amount}")
+
+# let´s ask from the user if they want to throw again
+question = input("Do you want to throw again Y/N?\n"")
